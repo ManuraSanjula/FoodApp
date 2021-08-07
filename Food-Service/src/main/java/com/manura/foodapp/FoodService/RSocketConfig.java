36a6a3672ceb01d1f0f49fb8d5edbccdf8cf73f1
Manura <w.m.manurasanjula12345@gmail.com>
2021-08-07 12:20:56 +0530

    Rename The User Service 1

---
+++ Food-Service/src/main/java/com/manura/foodapp/FoodService/RSocketConfig.java
@@ -0,0 +1,36 @@
+package com.manura.foodapp.FoodService;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.cbor.Jackson2CborDecoder;
+import org.springframework.http.codec.cbor.Jackson2CborEncoder;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+@Configuration
+public class RSocketConfig {
+	@Value("${food.service.port}")
+	int port;
+
+	@Bean
+	public RSocketStrategies rSocketStrategies() {
+		return RSocketStrategies.builder().encoders(encoders -> encoders.add(new Jackson2CborEncoder()))
+				.decoders(decoders -> decoders.add(new Jackson2CborDecoder())).build();
+	}
+
+	@Bean
+	public Mono<RSocketRequester> getRSocketRequester(RSocketRequester.Builder builder) {
+		return builder
+				.rsocketConnector(
+						rSocketConnector -> rSocketConnector.reconnect(Retry.fixedDelay(2, Duration.ofSeconds(2))))
+				.connect(TcpClientTransport.create(port));
+	}
+
+}
