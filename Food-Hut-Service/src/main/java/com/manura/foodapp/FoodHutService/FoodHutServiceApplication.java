f632f5abe95c5e25792e5a164c048f5b16b2229f
Manura <w.m.manurasanjula12345@gmail.com>
2021-08-11 23:55:23 +0530

    Aug 11th 5th add Food-Hut-Service commit

---
+++ Food-Hut-Service/src/main/java/com/manura/foodapp/FoodHutService/FoodHutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manura.foodapp.FoodHutService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodHutServiceApplication.class, args);
+	}
+
+}
