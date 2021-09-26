c48fb48895813056febff8c9e5507067f439d2f9
manura-sanjula <manura-sanjula@manura-sanjula>
2021-09-26 19:00:30 +0530

    Sep 26th Commit

--- Notification-Messages/src/main/java/com/manura/foodapp/NotificationMessages/NotificationMessagesApplication.java
+++ Notification-Messages/src/main/java/com/manura/foodapp/NotificationMessages/NotificationMessagesApplication.java
@@ -1,8 +1,8 @@
 package com.manura.foodapp.NotificationMessages;
 
 import org.redisson.Redisson;
-import org.redisson.api.RListReactive;
-import org.redisson.api.RTopic;
+import org.redisson.api.RSetReactive;
+import org.redisson.api.RTopicReactive;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.listener.MessageListener;
 import org.redisson.config.Config;
@@ -27,21 +27,37 @@ public class NotificationMessagesApplication {
 		redisson = Redisson.create(config);
 		
 		SpringApplication.run(NotificationMessagesApplication.class, args);
-		RTopic topic = redisson.getTopic("user_verify_email");
-		
-		topic.addListener(String.class, new MessageListener<String>() {
+		RTopicReactive user_topic1 = redisson.reactive().getTopic("user_verify_email");
+		user_topic1.addListener(String.class, new MessageListener<String>() {
 
 			@Override
 			public void onMessage(CharSequence channel, String msg) {
 				ObjectMapper objectMapper = new ObjectMapper();
 				try {
 					OtherUserEvents userEvents = objectMapper.readValue(msg,OtherUserEvents.class);
-					RListReactive<OtherUserEvents> list = redisson.reactive().getList("notification:" + userEvents.getUser());
-					list.add(userEvents).subscribe(i->{
-						System.out.println(i);
+					RSetReactive<String> list = redisson.reactive().getSet("notification:" + userEvents.getUser());
+					list.add(msg).subscribe(i->{
+						
 					});
 				}catch (Exception e) {
-					// TODO: handle exception
+					
+				}
+			}
+		  
+		});
+		RTopicReactive user_topic2 = redisson.reactive().getTopic("user_password_reset_success");
+		user_topic2.addListener(String.class, new MessageListener<String>() {
+			@Override
+			public void onMessage(CharSequence channel, String msg) {
+				ObjectMapper objectMapper = new ObjectMapper();
+				try {
+					OtherUserEvents userEvents = objectMapper.readValue(msg,OtherUserEvents.class);
+					RSetReactive<String> list = redisson.reactive().getSet("notification:" + userEvents.getUser());
+					list.add(msg).subscribe(i->{
+						
+					});
+				}catch (Exception e) {
+					
 				}
 			}
 		  
--- Notification-Messages/src/main/java/com/manura/foodapp/NotificationMessages/Service/NotificationService.java
+++ Notification-Messages/src/main/java/com/manura/foodapp/NotificationMessages/Service/NotificationService.java
@@ -1,18 +1,18 @@
 package com.manura.foodapp.NotificationMessages.Service;
 
 import org.redisson.Redisson;
-import org.redisson.api.RListReactive;
+import org.redisson.api.RSetReactive;
 import org.redisson.api.RTopicReactive;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.RedissonReactiveClient;
+import org.redisson.api.listener.MessageListener;
+import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.manura.foodapp.NotificationMessages.model.OtherUserEvents;
 
@@ -48,13 +48,45 @@ public class NotificationService implements WebSocketHandler {
 			Flux<WebSocketMessage> flux = Flux.empty();
 			return webSocketSession.send(flux);
 		}
-		RListReactive<OtherUserEvents> list = client.getList("notification:" + room);
-		RTopicReactive user_topic = client.getTopic(room);
+		RTopicReactive topic = client.getTopic(room, StringCodec.INSTANCE);
+		RSetReactive<String> list = client.getSet("notification:" + room);
 
-		
-		
+		RTopicReactive user_topic1 = client.getTopic("user_verify_email");
+		user_topic1.addListener(String.class, new MessageListener<String>() {
+			@Override
+			public void onMessage(CharSequence channel, String msg) {
+				ObjectMapper objectMapper = new ObjectMapper();
+				try {
+					OtherUserEvents userEvents = objectMapper.readValue(msg, OtherUserEvents.class);
+					if (userEvents.getUser().equals(room)) {
+						list.add(msg).then(topic.publish(msg)).subscribe(i -> {
 
-		return webSocketSession.send(Flux.empty());//
+						});
+					}
+				} catch (Exception e) {
+				}
+			}
+		});
+		RTopicReactive user_topic2 = client.getTopic("user_password_reset_success");
+		user_topic2.addListener(String.class, new MessageListener<String>() {
+			@Override
+			public void onMessage(CharSequence channel, String msg) {
+				ObjectMapper objectMapper = new ObjectMapper();
+				try {
+					OtherUserEvents userEvents = objectMapper.readValue(msg, OtherUserEvents.class);
+					if (userEvents.getUser().equals(room)) {
+						list.add(msg).then(topic.publish(msg)).subscribe(i -> {
+
+						});
+					}
+				} catch (Exception e) {
+				}
+			}
+		});
+		Flux<WebSocketMessage> flux = topic.getMessages(String.class).startWith(list.iterator())
+				.map(webSocketSession::textMessage).doOnError(System.out::println);
+
+		return webSocketSession.send(flux);
 	}
 
 	private String getChatRoomName(WebSocketSession socketSession) {
