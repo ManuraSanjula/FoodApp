4e51226baba85d538c455aa86d6144bbb4caa92f
manura-sanjula <manura-sanjula@manura-sanjula>
2021-09-30 09:49:35 +0530

    RSocket ApiGate-Way-Commit two

---
+++ RSocketApiGateWay/src/main/java/com/manura/foodapp/RSocketApiGateWay/RSocketApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.manura.foodapp.RSocketApiGateWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RSocketApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RSocketApiGateWayApplication.class, args);
+	}
+
+}
---
+++ RSocketApiGateWay/src/test/java/com/manura/foodapp/RSocketApiGateWay/RSocketApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manura.foodapp.RSocketApiGateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RSocketApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
