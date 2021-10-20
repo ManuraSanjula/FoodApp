78f00041950c06b8dddefa77205306487aab8450
Manura Sanjula <Manura Sanjula@LAPTOP-2MIJHAB9>
2021-10-20 17:58:37 +0530

    Add Food-App Config Server

---
+++ Food-App-Config-Server/src/main/java/com/manura/foodapp/FoodAppConfigServer/FoodAppConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.manura.foodapp.FoodAppConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAppConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppConfigServerApplication.class, args);
+	}
+
+}
---
+++ Food-App-Config-Server/src/test/java/com/manura/foodapp/FoodAppConfigServer/FoodAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manura.foodapp.FoodAppConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
