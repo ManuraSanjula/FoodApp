e9ec910250ac009fdb854c8740263a834ca205b6
Manura Sanjula <Manura Sanjula@LAPTOP-2MIJHAB9>
2021-10-21 18:06:33 +0530

    Add encryption

--- Food-App-Config-Server/src/main/java/com/manura/foodapp/FoodAppConfigServer/FoodAppConfigServerApplication.java
+++ Food-App-Config-Server/src/main/java/com/manura/foodapp/FoodAppConfigServer/FoodAppConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.manura.foodapp.FoodAppConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class FoodAppConfigServerApplication {
 
 	public static void main(String[] args) {
