0d42c5c121cc63b4510a90a398bf2962b90bc897
__Manux__ <58974680+ManuraSanjula@users.noreply.github.com>
2022-01-21 06:52:41 +0530

    21/01/2022  Commit

--- UserMicroService/src/test/java/com/manura/foodapp/UserServiceTest.java
+++ UserMicroService/src/test/java/com/manura/foodapp/UserServiceTest.java
@@ -1,7 +1,5 @@
 package com.manura.foodapp;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
